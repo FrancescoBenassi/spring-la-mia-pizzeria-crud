2f0a7936aafcd5abe7551cc13d93edf76d126e75
FrancescoBenassi <francybenna77@gmail.com>
2025-02-17 19:00:36 +0100

    feat: added project

---
+++ src/main/java/org/exercise/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -0,0 +1,42 @@
+package org.exercise.spring_la_mia_pizzeria_crud.controller;
+
+import java.util.List;
+
+import org.exercise.spring_la_mia_pizzeria_crud.model.Pizza;
+import org.exercise.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/pizza")
+public class PizzaController {
+
+    @Autowired
+    private PizzaRepository repository;
+
+    @GetMapping
+    public String index(Model model) {
+        List<Pizza> pizzas = repository.findAll();
+        model.addAttribute("pizzas", pizzas);
+        return "pizza/index";
+    }
+
+    // @GetMapping("/search/{name}")
+    // public String findByKeyword(@RequestParam(name = "query") String query, Model model) {
+    //     List<Pizza> pizzas = repository.findByTitleContaining(query);
+    //     model.addAttribute("pizzas", pizzas);
+    //     return "pizza/index";
+    // }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        // Pizza pizza = repository.findById(id).get();
+        model.addAttribute("pizza", repository.findById(id).get());
+        return "pizza/showPizza";
+    }
+}
---
+++ src/main/java/org/exercise/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -0,0 +1,81 @@
+package org.exercise.spring_la_mia_pizzeria_crud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "pizza")
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Name must not be null")
+    private String name;
+
+    @NotBlank(message = "Description must not be null")
+    private String description;
+
+    @NotBlank(message = "Image must not be null")
+    @Lob
+    private String image;
+
+    @NotNull
+    @Min(value = 0, message = "Price cannot be a negative number")
+    private Integer price;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return this.image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Integer getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+       
+        return this.id + ' ' + this.name + ' ' + this.description + ' ' + this.image + ' ' + this.price;
+    }
+
+}
