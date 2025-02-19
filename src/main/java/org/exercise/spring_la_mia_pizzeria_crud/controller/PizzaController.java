90479ec2210bbce8797b8f88eb1cdf17c2f6e14d
FrancescoBenassi <francybenna77@gmail.com>
2025-02-19 12:45:26 +0100

    feat PizzaController: added create get and post

--- src/main/java/org/exercise/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/exercise/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -7,10 +7,18 @@ import org.exercise.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/pizza")
@@ -39,4 +47,24 @@ public class PizzaController {
         model.addAttribute("pizza", repository.findById(id).get());
         return "pizza/showPizza";
     }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("pizza", new Pizza());
+        return "pizza/create";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("pizza") Pizza pizzaForm, BindingResult bindingResult, Model model,
+            RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            return "pizza/create";
+        }
+
+        repository.save(pizzaForm);
+        redirectAttributes.addFlashAttribute("message", "A new pizza has been added");
+        return "redirect:/pizza";
+    }
+
 }
