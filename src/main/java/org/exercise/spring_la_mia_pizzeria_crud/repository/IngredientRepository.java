dc933361b6043d6b662a5e6da7d3b83a114931eb
FrancescoBenassi <francybenna77@gmail.com>
2025-03-01 16:24:59 +0100

    feat: added IngredientRepository

---
+++ src/main/java/org/exercise/spring_la_mia_pizzeria_crud/repository/IngredientRepository.java
@@ -0,0 +1,9 @@
+package org.exercise.spring_la_mia_pizzeria_crud.repository;
+
+import org.exercise.spring_la_mia_pizzeria_crud.model.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Integer>{
+    
+    
+}
