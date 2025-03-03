b4468d4d5228fa1cb29d23efcc30f7704b55e74d
FrancescoBenassi <francybenna77@gmail.com>
2025-03-03 19:11:15 +0100

    feat PizzaService: added methods

--- src/main/java/org/exercise/spring_la_mia_pizzeria_crud/service/PizzaService.java
+++ src/main/java/org/exercise/spring_la_mia_pizzeria_crud/service/PizzaService.java
@@ -1,5 +1,8 @@
 package org.exercise.spring_la_mia_pizzeria_crud.service;
 
+import java.util.List;
+
+import org.exercise.spring_la_mia_pizzeria_crud.model.Ingredient;
 import org.exercise.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.exercise.spring_la_mia_pizzeria_crud.model.SpecialOffer;
 import org.exercise.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
@@ -16,8 +19,27 @@ public class PizzaService {
     @Autowired
     SpecialOfferRepository specialOfferRepository;
 
-    public void deleteById( Pizza pizza){
+    public Pizza getById(Integer id) {
+        return pizzaRepository.findById(id).get();
+    }
 
+   public List<Pizza> findByName(String name) {
+        return pizzaRepository.findByNameContainingIgnoreCase(name);
+    }
+
+    public List<Pizza> findAll(){
+        return pizzaRepository.findAll();
+    }
+
+    public Pizza create(Pizza pizza) {
+        return pizzaRepository.save(pizza);
+    }
+
+    public Pizza update(Pizza pizza) {
+        return pizzaRepository.save(pizza);
+    }
+
+    public void deleteById( Pizza pizza){
         for (SpecialOffer specialOffer : pizza.getSpecialOffers()) {
             specialOfferRepository.delete(specialOffer);
         }
